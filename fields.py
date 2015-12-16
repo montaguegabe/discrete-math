9ccd9d25167f57f6ca06d12075180379ef3831f3
Gabe Montague <gmontague@college.harvard.edu>
2015-12-16 02:09:53 -0500

    Fixed log table generation

--- fields.py
+++ fields.py
@@ -4,12 +4,12 @@ class Field(object):
         super(Field, self).__init__()
 
     # Gets the identities
-    @staticmethod
-    def mult_id():
+    @classmethod
+    def mult_id(cls):
         raise NotImplementedError
 
-    @staticmethod
-    def add_id():
+    @classmethod
+    def add_id(cls):
         raise NotImplementedError
 
 # Define a normal number in the real numbers
@@ -18,19 +18,19 @@ class R(float):
         try: return float.__new__(cls, float_string)
         except (TypeError, ValueError): raise ValueError("Could not make float out of real number")
 
-    @staticmethod
-    def mult_id():
+    @classmethod
+    def mult_id(cls):
         return 1
 
-    @staticmethod
-    def add_id():
+    @classmethod
+    def add_id(cls):
         return 0
 
 # The parent class of all finite fields. Uses logarithm tables only for speed
 class FiniteField(Field):
 
     # The characteristic
-    char = None
+    characteristic = None
 
     # Number of copies of the characteristic cyclic group, max power + 1
     r = None
@@ -46,8 +46,8 @@ class FiniteField(Field):
     irreducible_poly = None
 
     # Define a polynomial multiplier that does it the hard way
-    @staticmethod
-    def poly_mult(p1, p2):
+    @classmethod
+    def poly_mult(cls, p1, p2):
 
         # Init the new polynomial
         order1 = len(p1) - 1
@@ -59,84 +59,97 @@ class FiniteField(Field):
         for power1, coef1 in enumerate(p1):
             for power2, coef2 in enumerate(p2):
                 new_power = power1 + power2
-                new_coef = (coef1 * coef2) % characteristic
+                new_coef = (coef1 * coef2) % cls.characteristic
                 result[new_power] += new_coef
 
         # Reduce by the polynomial
-        irreducible_poly_order = len(irreducible_poly) - 1
+        irreducible_poly_order = len(cls.irreducible_poly) - 1
         if new_order > irreducible_poly_order:
             raise ValueError("Poly mult is not meant for this!")
 
-        x_to_order_equiv_neg = irreducible_poly[:-1]
-        factor = -result[-1]
-        for power in xrange(len(x_to_order_equiv_neg)):
-            result[power] += (x_to_order_equiv_neg[power] * factor)
+        if new_order == irreducible_poly_order:
+            x_to_order_equiv_neg = cls.irreducible_poly[:-1]
+            factor = -result[irreducible_poly_order]
+            del result[irreducible_poly_order]
+            for power in xrange(len(result)):
+                result[power] = (result[power] + (x_to_order_equiv_neg[power] * factor)) % cls.characteristic
+
+            # Trim zeros off the end
+            for i in xrange(len(result) - 1, 0, -1):
+                if result[i] == 0:
+                    del result[i]
+                else:
+                    break
 
         return result
 
     # Gets the size of the field
-    @staticmethod  
-    def size():
-        return char ** r
+    @classmethod  
+    def size(cls):
+        return cls.characteristic ** cls.r
 
     # Gets all elements of the field (as an iterable). Returns in order of primitive powers
-    @staticmethod
-    def all_values():
+    @classmethod
+    def all_values(cls):
 
         # Yield the additive identity first
-        yield add_id()
+        yield cls("0")
 
         # Then yield the multiplicative identity
-        yield mult_id()
+        yield cls("1")
 
         # Iterate through the rest of the multiplicative group
-        size_f_mult = size() - 1
-        value = mult_id()
+        size_f_mult = cls.size() - 1
+        value = cls("1")
 
         # Yield the subsequent powers
         iteration = 1
         while iteration < size_f_mult:
 
-            value = value * primitive
-            if value == mult_id:
+            # Compute the next element
+            new_value = cls("0")
+            new_value.coefficients = cls.poly_mult(value.coefficients, cls.primitive)
+            value = new_value
+
+            if value == cls.mult_id:
                 raise ValueError("Invalid primitive element")
             yield value
             iteration += 1
 
     # Gets the logarithm table for the finite field
-    @staticmethod
-    def get_log_table():
+    @classmethod
+    def get_log_table(cls):
 
         # Lazily compute the logarithm table
-        if log_table == None:
+        if cls.log_table == None:
 
             log_table = []
             log_table_reverse = {}
 
             power = 0
 
-            for value in all_values():
+            # Store finite field members in the table - not polynomial lists
+            for value in cls.all_values():
 
                 # The additive identity is not included in the log table
-                if value == add_id: continue
+                if value == cls.add_id(): continue
                 log_table.append(value)
                 log_table_reverse[value] = power
                 power += 1
 
-        return log_table
+            cls.log_table = log_table
+
+        return cls.log_table
 
     # Maps field members to primitive order
-    @staticmethod
-    def get_log_table_reverse():
-        get_log_table()
+    @classmethod
+    def get_log_table_reverse(cls):
+        self.get_log_table()
         return log_table_reverse
 
     # Create a polynomial from a string
     def __init__(self, string):
         super(FiniteField, self).__init__()
-
-        # Compute the log table if haven't already
-        get_log_table()
 
         # Store polynomial coefficients (of instances)
         self.coefficients = []
@@ -144,21 +157,20 @@ class FiniteField(Field):
         compact = string.replace(" ", "")
 
         # Handle zero
-        if compact != "0":
+        if compact == "0":
             self.coefficients.append(0)
 
         # Parse terms
         else:
-            terms = text.split("+")
+            terms = compact.split("+")
             degree = -1
             for term in list(reversed(terms)):
-
                 coef = None
                 power = None
 
                 # Attempt to split cleanly into coef and power
-                coef_power = "x^".split(term)
-                if (len(coef_power > 1)):
+                coef_power = filter(None, term.split("x^"))
+                if (len(coef_power) > 1):
                     coef = int(coef_power[0])
                     power = int(coef_power[1])
 
@@ -166,16 +178,24 @@ class FiniteField(Field):
                     x_index = term.find("x")
 
                     if (x_index == -1):
-
                         # We simply have a constant
                         coef = int(term)
                         power = 0
 
                     else:
+                        # There is an x
+                        if term.find("^") == -1:
 
-                        # We only have a power
-                        coef = 1
-                        power = 1 if term.find("^") == -1 else int(term[2:])
+                            # Power is 1
+                            power = 1
+                            coef_string = term[:-1]
+                            if coef_string == "-": coef_string = "-1"
+                            coef = int(coef_string) if term[:-1] else 1
+                        else:
+                            # Coefficient is 1
+                            coef = 1
+                            power = int(term[2:])
+
 
                 if coef == None or power == None:
                     raise ValueError("Could not parse input polynomial")
@@ -183,14 +203,16 @@ class FiniteField(Field):
                 # Add the term, first doing some padding with 0 coefficients if necessary
                 while degree < power - 1:
                     self.coefficients.append(0)
-                self.coef_power.append(coef)
+                    degree += 1
+                self.coefficients.append(coef)
+                degree = power
 
     # Adds two field members together
     def __add__(self, other):
         result = copy.deepcopy(self)
 
         for power, coef in enumerate(other.coefficients):
-            result[power] = (result[power] + coef) % characteristic
+            result[power] = (result[power] + coef) % type(self).characteristic
 
         return result
 
@@ -198,7 +220,7 @@ class FiniteField(Field):
     def __hash__(self):
         val = 0
         for power, coef in enumerate(self.coefficients):
-            val += (characteristic ** power) * coef
+            val += (type(self).characteristic ** power) * coef
         return val
 
     # Multiplies two field members together
@@ -236,16 +258,55 @@ class FiniteField(Field):
                 written_terms.append(term)
 
         # No terms means zero
-        if not terms: return "0"
+        if not written_terms: return "0"
 
-        return " + ".join(terms)
+        return " + ".join(written_terms)
 
     def __repr__(self):
         return self.__class__.__name__ + "(" + self.__unicode__() + ")"
 
 
-
-
 # Define F4
+class F4(FiniteField):
 
-field_tests:
+    # The characteristic
+    characteristic = 2
+
+    # Number of copies of the characteristic cyclic group, max power + 1
+    r = 2
+
+    # The generator of the logarithm table
+    primitive = [0, 1] # x
+
+    # The logarithm table of the field. Access with method instead
+    log_table = None
+    log_table_reverse = None # Matches field members to generator orders
+
+    # The irreducible polynomial of the field. Must have leading coefficient of 1
+    irreducible_poly = [1, 1, 1]
+
+    # Gets the identities
+    @classmethod
+    def mult_id(cls):
+        return F4("1")
+
+    @classmethod
+    def add_id(cls):
+        return F4("0")
+
+    def __init__(self, string):
+        super(F4, self).__init__(string)
+        
+
+def field_tests():
+
+    a = F4("x + 1")
+    b = F4("x")
+    print a
+    print b
+
+    print F4.get_log_table()
+    
+
+
+field_tests()
